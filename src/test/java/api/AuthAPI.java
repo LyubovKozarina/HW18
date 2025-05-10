80e1a4999e0274ba7f3ba2ef95114100f8a004b7
LSKozarina <lubov.kozarina@pix.ru>
2025-05-10 22:20:41 +0300

    added TestBase and WithLogin

---
+++ src/test/java/api/AuthAPI.java
@@ -0,0 +1,23 @@
+package api;
+
+import models.LoginRequestModel;
+import models.LoginResponseModel;
+import specs.LoginSpec;
+import tests.TestData;
+
+import static io.restassured.RestAssured.given;
+
+public class AuthAPI{
+
+    public static LoginResponseModel login() {
+        LoginRequestModel request = new LoginRequestModel(TestData.login,TestData.password);
+        return
+                given(LoginSpec.request)
+                        .body(request)
+                        .when()
+                        .post("/Account/v1/Login")
+                        .then()
+                        .spec(LoginSpec.response)
+                        .extract().as(LoginResponseModel.class);
+    }
+}
