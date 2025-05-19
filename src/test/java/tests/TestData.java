6c65c0acecce84aa4264e81aaf320b67adf190f9
LSKozarina <lubov.kozarina@pix.ru>
2025-05-19 16:53:44 +0300

    changed TestData

--- src/test/java/tests/TestData.java
+++ src/test/java/tests/TestData.java
@@ -2,7 +2,7 @@ package tests;
 
 public class TestData {
 
-    public static final String login = System.getProperty("login", "default_user");
-    public static final String password = System.getProperty("password", "default_pass");
+    public static final String login = System.getProperty("login");
+    public static final String password = System.getProperty("password");
     public static final String isbn = "9781491904244";
 }
